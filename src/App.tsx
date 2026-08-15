58f1af0cfbcb550f2446893bb9bcf387c0092b8e
Sourav <souravsahoo407@gmail.com>
2026-08-16 00:17:50 +0530

    minior change

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,51 @@
-
-  import { Outlet } from "react-router-dom"
-  import Footer from "./components/Footer/Footer"
-  import Header from "./components/Header/Header"
+import Contact from './components/Contact/Contact'
+import Home from './components/Home/Home'
+import  About  from './components/About/About'
+import Github from './components/Github/Github'
+import { GithubInfoLoader } from './components/Github/Github'
+import { RouterProvider,createBrowserRouter } from 'react-router-dom'
+import { Layout } from './components/Layout/Layout'
+ 
 
 export const App = () => {
+
+
+
+
+const router = createBrowserRouter([
+
+{
+  path : '/',
+  element : <Layout/>,
+  children : [
+    {
+      path : "",
+      element : <Home/>
+    },
+    {
+      path : "about",
+      element : <About/>
+    },
+    {
+      loader:GithubInfoLoader ,
+
+      path:"github",
+      element : <Github/>
+    },
+    {
+      path:"contact",
+      element:<Contact/>
+    }
+    
+  ]
+}
+
+])
+
   return (
 
      <>
-  <Header/>
-  <Outlet/>
-  <Footer/>
+ <RouterProvider router={(router)}/>
 
      </>
   )
--- src/main.tsx
+++ src/main.tsx
@@ -1,53 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { RouterProvider,createBrowserRouter } from 'react-router-dom'
+
 
 
 import { App } from './App'
-import Contact from './components/Contact/Contact'
-import Home from './components/Home/Home'
-import  About  from './components/About/About'
-import Github from './components/Github/Github'
-import { GithubInfoLoader } from './components/Github/Github'
-
-
-const router = createBrowserRouter([
-
-{
-  path : '/',
-  element : <App/>,
-  children : [
-    {
-      path : "",
-      element : <Home/>
-    },
-    {
-      path : "about",
-      element : <About/>
-    },
-    {
-      loader:GithubInfoLoader ,
-
-      path:"github",
-      element : <Github/>
-    },
-    {
-      path:"contact",
-      element:<Contact/>
-    }
-    
-  ]
-}
-
-])
 
 
 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={(router)}/>
+    
  
   </StrictMode>,
 )
