7f45dcdc24df1a23e53d101f3ec2b4e070d55b9e
Sourav <souravsahoo407@gmail.com>
2026-08-18 23:18:34 +0530

    cd change

---
+++ src/components/ThemeToggle.tsx
@@ -0,0 +1,67 @@
+
+import { ThemeProvider } from "./context/Theme";
+import { useState , useEffect } from "react"
+import ThemeButton from "./button";
+import Card from "./card";
+
+
+export const ThemeToggle = () => {
+
+  const [ThemeMode,setThemeMode] = useState("light") ;
+
+  
+
+const lightTheme = ()=> {
+
+  setThemeMode("light")
+}
+
+const darkTheme = ()=> {
+  setThemeMode("dark")
+}
+
+
+// actual theme change using the Context Api
+
+
+useEffect(()=>{
+  document.querySelector('html')?.classList.remove("light","dark");
+  document.querySelector("html")?.classList.add(ThemeMode)
+
+
+},[ThemeMode]
+)
+
+
+  return (
+
+  <ThemeProvider value={{ ThemeMode ,darkTheme,lightTheme}}>
+
+    <div
+        className={`flex min-h-screen w-full flex-wrap items-center transition-colors duration-300 ${
+          ThemeMode === "dark"
+            ? "bg-gray-900"
+            : "bg-white"
+        }`}
+      >
+      <div className="w-full">
+         <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+  
+              {<ThemeButton/> }
+         </div>
+
+         <div className = "w-full max-w-sm mx-auto"
+         >
+          {<Card/>}
+         </div>
+
+      </div>
+
+
+    </div>
+    </ThemeProvider>
+      
+   
+    
+  )
+}
