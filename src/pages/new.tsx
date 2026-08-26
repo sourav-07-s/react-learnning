c1548b0782d1687b9a463121fd1b49326d68a553
Sourav <souravsahoo407@gmail.com>
2026-08-27 00:15:46 +0530

    changes

--- src/pages/new.tsx
+++ src/pages/new.tsx
@@ -2,16 +2,6 @@ import React from 'react'
 
 export const New = () => {
   return (
-    <>
-     <nav> 
 
-        <ul>
-            <li>Home </li>
-            <li> contact me </li>
-            <li> about </li>
-        </ul>
-     </nav>
-    
-    </>
   )
 }
